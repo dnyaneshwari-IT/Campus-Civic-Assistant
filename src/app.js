6840858576be76897167032bc7e9e50db92349cb
dnyaneshwari-IT <dnyaneshwarighodekar15@gmail.com>
2026-08-19 14:20:51 +0530

    Enable CORS for frontend

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+
 const authRoutes = require("./routes/auth.routes");
 const categoryRoutes = require("./routes/category.routes");
 const departmentRoutes = require("./routes/department.routes");
 const issueRoutes = require("./routes/issue.routes");
 const adminRoutes = require("./routes/admin.routes");
 
-
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
 
 app.use(express.json());
 
